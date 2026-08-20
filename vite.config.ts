1b841acbc44d378c2aef619e664eb195080b1242
irvan-byte <54943060+irvan-byte@users.noreply.github.com>
2026-08-21 00:48:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -206,6 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
+  base: "/eportfolio-ppg-irvan/",
   plugins,
   resolve: {
     alias: {
