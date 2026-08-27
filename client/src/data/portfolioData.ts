ed665631e1653f1b52f67d01f827f8e9914f7646
irvan-byte <54943060+irvan-byte@users.noreply.github.com>
2026-08-28 04:02:10 +0700

    Add archive URL to portfolio data

--- client/src/data/portfolioData.ts
+++ client/src/data/portfolioData.ts
@@ -163,6 +163,7 @@ dokumentasi: [
     ],
     refleksi: "Saya menyadari bahwa guru sejati adalah among yang menuntun kodrat anak, bukan menuntut anak sesuai kehendak guru.",
     dokumentasi: ["Esai reflektif belum ditambahkan", "Presentasi kelompok belum ditambahkan"]
+    arsipUrl: "https://drive.google.com/drive/folders/1f8FVLS1DvW3sJTPx7E-AIDGBj_Vc0Rvy?usp=sharing",
   },
   {
     id: "kbg-1",
